05bee412d56196da2ba764447ef700bedb86e032
Eszter Szluka <szluka.eszter@gmail.com>
2019-10-01 16:15:50 +0200

    add interface OrderDao

--- src/main/java/com/codecool/shop/model/Order.java
+++ src/main/java/com/codecool/shop/model/Order.java
@@ -6,14 +6,15 @@ import java.util.List;
 public class Order {
 
     private List<LineItem> items;
+    private int id;
 
-    public Order(Product product){
+    public Order(Product product) {
         this.items = new ArrayList<>();
         items.add(new LineItem(product, 1));
     }
 
     public void addProduct(Product product) {
-        for (LineItem item: items) {
+        for (LineItem item : items) {
             if (item.getProduct().equals(product)) {
                 item.increaseQuantity();
                 return;
