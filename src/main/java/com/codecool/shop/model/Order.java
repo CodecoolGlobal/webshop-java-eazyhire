c15ad94ead590b2598dcca28da58c69d071d4e68
Panna Kristóf <kristofpanna@gmail.com>
2019-10-02 17:29:29 +0200

    Display sum price of cart

--- src/main/java/com/codecool/shop/model/Order.java
+++ src/main/java/com/codecool/shop/model/Order.java
@@ -1,16 +1,20 @@
 package com.codecool.shop.model;
 
 import java.util.ArrayList;
+import java.util.Currency;
 import java.util.List;
 
 public class Order {
 
     private List<LineItem> items;
     private int id;
+    private Currency currency;
+
 
     public Order(Product product) {
         this.items = new ArrayList<>();
         items.add(new LineItem(product, 1));
+        currency = product.getDefaultCurrency();
     }
 
     public void addProduct(Product product) {
@@ -41,6 +45,18 @@ public class Order {
 
     public List<LineItem> getItems() {
         return new ArrayList<>(items);
+    }
+
+    public float getTotalPrice() {
+        float sum = 0;
+        for (LineItem item : items) {
+            sum += item.getTotalPrice();
+        }
+        return sum;
+    }
+
+    public String getTotalPriceString() {
+        return getTotalPrice() + " " + currency.toString();
     }
 
     @Override
