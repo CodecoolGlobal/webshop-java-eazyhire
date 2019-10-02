cfe37475b3fe17368f10b83232261458761f004a
Panna Kristóf <kristofpanna@gmail.com>
2019-10-02 21:06:52 +0200

    Update number in header on adding an item

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -7,13 +7,20 @@ function init(){
     }
 }
 
-function addToCart (event) {
+function increaseCartSumQuantityDisplay() {
+    let cartSumQuantityDisplay = document.querySelector(".cart-sumquantity");
+    let actQuantity = parseInt(cartSumQuantityDisplay.dataset.sumquantity) + 1;
+    cartSumQuantityDisplay.textContent = "Cart: " + actQuantity;
+    cartSumQuantityDisplay.dataset.sumquantity = actQuantity.toString();
+}
 
+function addToCart (event) {
     let button = event.target;
     let productId = button.dataset["productid"];
     let data = "productId=" + productId;
-    console.log(data);
     dataHandler.sendData(data, '/');
+
+    increaseCartSumQuantityDisplay();
 }
 
 init();
