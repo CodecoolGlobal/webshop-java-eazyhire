461dfb7b0bf8ee79c423e07caf5730b432c06ab4
Marton Csizmazia <csmarton93@gmail.com>
2019-10-17 15:26:58 +0200

    ProductcatergoryDaoDB test implemented

---
+++ src/main/java/com/codecool/shop/dao/implementation/db/ProductCategoryDaoDB.java
@@ -0,0 +1,75 @@
+package com.codecool.shop.dao.implementation.db;
+
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.model.ProductCategory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class ProductCategoryDaoDB implements ProductCategoryDao {
+    DbCreator dbCreator = new DbCreator();
+
+    @Override
+    public void add(ProductCategory category) {
+        String query = "" +
+                "INSERT INTO product_category (name, department, description) " +
+                "VALUES (?, ?, ?)" +
+                "RETURNING id; ";
+        try (
+                Connection connection = dbCreator.getConnection();
+                PreparedStatement ps = connection.prepareStatement(query);
+        ) {
+            ps.setString(1, category.getName());
+            ps.setString(2, category.getDepartment());
+            ps.setString(3, category.getDescription());
+
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            category.setId(rs.getInt("id"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public ProductCategory find(int id) {
+        String query = "" +
+                "SELECT * FROM product_category " +
+                "WHERE id = ?;";
+        try (
+                Connection connection = dbCreator.getConnection();
+                PreparedStatement ps = connection.prepareStatement(query);
+        ) {
+            ps.setInt(1, id);
+
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+
+            ProductCategory found = new ProductCategory(
+                    rs.getString("name"),
+                    rs.getString("department"),
+                    rs.getString("description")
+            );
+            found.setId(rs.getInt("id"));
+
+            return found;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<ProductCategory> getAll() {
+        return null;
+    }
+}
---
+++ src/test/java/com/codecool/shop/dao/implementation/db/ProductCategoryDaoDBTest.java
@@ -0,0 +1,28 @@
+package com.codecool.shop.dao.implementation.db;
+
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.model.ProductCategory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ProductCategoryDaoDBTest {
+
+    @BeforeEach
+    void resetDB(){
+        DbCreator dbCreator = new DbCreator();
+        dbCreator.executeUpdateFromFile("/src/main/java/com/codecool/shop/dao/implementation/db/sql/db_init.sql");
+
+    }
+
+    @Test
+    void addAndFind_nameEquals() {
+        ProductCategory testProductCategory = new ProductCategory("test ProductCategory", "test_department","test desc of supp");
+        ProductCategoryDao productCategoryDao = new ProductCategoryDaoDB();
+        productCategoryDao.add(testProductCategory);
+
+        int newId = testProductCategory.getId();
+        assertEquals(testProductCategory.getName(), productCategoryDao.find(newId).getName());
+    }
+}
