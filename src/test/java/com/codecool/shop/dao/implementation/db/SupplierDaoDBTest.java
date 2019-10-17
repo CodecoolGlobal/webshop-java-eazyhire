45ac6e2cee7c6505a14b2cc773e502bfcd2073cf
Panna Kristóf <kristofpanna@gmail.com>
2019-10-17 17:51:40 +0200

    Add tests to test remove

--- src/test/java/com/codecool/shop/dao/implementation/db/SupplierDaoDBTest.java
+++ src/test/java/com/codecool/shop/dao/implementation/db/SupplierDaoDBTest.java
@@ -8,20 +8,42 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SupplierDaoDBTest {
+    private SupplierDao supplierDao;
+
     @BeforeEach
+    void setUp() {
+        resetDB();
+        supplierDao = new SupplierDaoDB();
+    }
+
     void resetDB(){
         DbCreator dbCreator = new DbCreator();
         dbCreator.executeUpdateFromFile("src/main/java/com/codecool/shop/dao/implementation/db/sql/db_init.sql");
-
     }
+
     @Test
     void addAndFind_nameEquals() {
         Supplier testSupplier = new Supplier("test supplier", "test desc of supp");
-        SupplierDao supplierDao = new SupplierDaoDB();
         supplierDao.add(testSupplier);
-
         int newId = testSupplier.getId();
+
         assertEquals(testSupplier.getName(), supplierDao.find(newId).getName());
     }
 
+    @Test
+    void findNonexistent_returnNull() {
+        SupplierDao supplierDao = new SupplierDaoDB();
+        assertNull(supplierDao.find(0));
+    }
+
+    @Test
+    void addRemoveFind_returnNull() {
+        Supplier testSupplier = new Supplier("test supplier", "test desc of supp");
+        supplierDao.add(testSupplier);
+        int newId = testSupplier.getId();
+
+        supplierDao.remove(newId);
+
+        assertNull(supplierDao.find(newId));
+    }
 }
