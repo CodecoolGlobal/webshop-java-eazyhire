c519815bebcebeede7423a2e1463452465cc3544
Panna Kristóf <kristofpanna@gmail.com>
2019-10-18 03:51:55 +0200

    Generic dao: ProductDao extends generic

--- src/main/java/com/codecool/shop/dao/ProductDao.java
+++ src/main/java/com/codecool/shop/dao/ProductDao.java
@@ -6,13 +6,7 @@ import com.codecool.shop.model.ProductCategory;
 
 import java.util.List;
 
-public interface ProductDao {
-
-    void add(Product product);
-    Product find(int id);
-    void remove(int id);
-
-    List<Product> getAll();
+public interface ProductDao extends BaseDao<Product> {
     List<Product> getBy(Supplier supplier);
     List<Product> getBy(ProductCategory productCategory);
 
